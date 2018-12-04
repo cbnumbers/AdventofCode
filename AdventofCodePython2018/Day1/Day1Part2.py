bdc7ed956a8b14f3357326ae9762f0377b35df12
cbnumbers <cboutte91@gmail.com>
2018-12-03 20:30:36 -0600

    save just in case

--- AdventofCodePython2018/Day1/Day1Part2.py
+++ AdventofCodePython2018/Day1/Day1Part2.py
@@ -1,6 +1,6 @@
 ####Advent of Code Day 1 Part 2
 ####Load input data
-inputdata = open("D:\\CodeProjects\\PythonStuff\\AdventofCodePython2018\\AdventofCodePython2018\\Day2\\AdventDay1Input.txt", "r").read().split()
+inputdata = open("D:\\CodeProjects\\PythonStuff\\AdventofCodePython2018\\AdventofCodePython2018\\AdventDay1Input.txt", "r").read().split()
 
 #### Create sequence integer array with sequence
 newsequence = []
@@ -11,9 +11,6 @@ frequency = 0 #maybe need user input for begining frequency
 resultlist = []
 cycles = 0
 
-
-### create while loop to check the count of the current frequency value
-### also counts number of cycles it took to find first dupe value
 while resultlist.count(frequency) != 2:
     cycles = cycles + 1
     print(cycles)
